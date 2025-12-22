07871d8085e6a6e6c67c822df76d5963bc7ea5c9
flrn <diablodvorak@gmail.com>
2025-12-22 21:09:04 +0700

    add flashing light. also this project development will be halted for an indefinite amount of time. stay tuned for the next development!

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 
 #include "renderer_subsystem.h"
 
+enum class Color {
+    Red,
+    Green,
+    Blue,
+};
+
 int32_t main(int32_t argc, char** argv)
 {
     auto window = WindowSubsystem::instance();
@@ -20,6 +26,16 @@ int32_t main(int32_t argc, char** argv)
         return -1;
     }
 
+    Color current_color = Color::Red;
+
+    float red_value = 0.0f;
+    float green_value = 0.0f;
+    float blue_value = 0.0f;
+
+    float red_factor = 0.1f;
+    float green_factor = 0.1f;
+    float blue_factor = 0.1f;
+
     while (!window->should_close()) {
         window->poll_events();
 
@@ -27,7 +43,27 @@ int32_t main(int32_t argc, char** argv)
         if (!frame)
             continue;
 
-        frame.begin(0.1f, 0.1f, 0.1f, 1.0f);
+        if (current_color == Color::Red) {
+            red_value += red_factor;
+            if (red_value >= 1.0f || red_value <= 0.0f) {
+                current_color = Color::Green;
+                red_factor *= -1.0f;
+            }
+        } else if (current_color == Color::Green) {
+            green_value += green_factor;
+            if (green_value >= 1.0f || green_value <= 0.0f) {
+                current_color = Color::Blue;
+                green_factor *= -1.0f;
+            }
+        } else {
+            blue_value += blue_factor;
+            if (blue_value >= 1.0f || blue_value <= 0.0f) {
+                current_color = Color::Red;
+                blue_factor *= -1.0f;
+            }
+        }
+
+        frame.begin(red_value, green_value, blue_value, 1.0f);
         frame.end();
         frame.submit_and_present();
     }
